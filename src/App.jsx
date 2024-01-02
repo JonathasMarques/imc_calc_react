5a06857ec720ddec2a1be1a274e7dcffb3a38fa3
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2024-01-02 13:49:55 -0300

    Projeto calculadora feita por React.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { data } from "./data/data"
+
+import { useState } from 'react';
+
+import ImcCalc from './components/ImcCalc';
+import ImcTable from './components/ImcTable'
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const calcImc = (e, height, weight) {
+    console.log("executou")
+  }
+
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        {!imc ? <ImcCalc calcImc={calcImc}/> : <ImcTable />}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
